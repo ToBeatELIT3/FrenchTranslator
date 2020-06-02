02e7902c459d8d39f92410ed9f4baa6b603b2dab
ToBeatELIT3 <amitojsingh8070@gmail.com>
2020-06-02 10:03:52 -0400

    Shortened Code By Creating Global Variables

--- scripts/englishword.py
+++ scripts/englishword.py
@@ -9,46 +9,41 @@ class EnglishWord:
         self.entofr_url = (f"https://www.linguee.com/english-french/search?source=auto&query={self.word}")
         self.en_url = (f"https://www.dictionary.com/browse/{self.word}?s=ts")
 
-    def getpagehtml(self):
-        with open(f"html/{self.word}_entofr_webpage.html", "w", encoding="utf-8") as filename: 
-            filename.write(f"{self.page_soup_entofr}")
+        uClient = uReq(self.entofr_url)
+        page_html = uClient.read()
+        uClient.close()
 
-        with open(f"html/{self.word}_en_webpage.html", "w", encoding="utf-8") as filename:
-            filename.write(f"{self.page_soup_en_url}")
+        self.page_soup_entofr = soup(page_html, "html.parser")
 
-    def getdefinition(self):
         uClient = uReq(self.en_url)
         page_html = uClient.read()
         uClient.close()
 
-        page_soup_en_url = soup(page_html, "html.parser")
+        self.page_soup_en_url = soup(page_html, "html.parser")
 
-        worddefinition = page_soup_en_url.find("span", class_="one-click-content css-1p89gle e1q3nk1v4")
+    def getpagehtml(self):
+    
+        with open(f"html/{self.word}_entofr_webpage.html", "w", encoding="utf-8") as filename: 
+            filename.write(f"{self.entofr_url}")
+
+        with open(f"html/{self.word}_en_webpage.html", "w", encoding="utf-8") as filename:
+            filename.write(f"{self.en_url}")
+
+    def getdefinition(self):
+        worddefinition =self.page_soup_en_url.find("span", class_="one-click-content css-1p89gle e1q3nk1v4")
         worddefinition = worddefinition.text.strip()
 
         print(f"The Definition of {self.word} is: {worddefinition}")
         return worddefinition
     
     def getexaples(self):
-        uClient = uReq(self.en_url)
-        page_html = uClient.read()
-        uClient.close()
-
-        page_soup_en_url = soup(page_html, "html.parser")
-
-        wordexaples = page_soup_en_url.findAll("p", class_="one-click-content css-a8m74p e15kc6du6")
+        wordexaples = self.page_soup_en_url.findAll("p", class_="one-click-content css-a8m74p e15kc6du6")
         
         for x in range(len(wordexaples)):
             print(f"Example {x}: {wordexaples[x].text.strip()}\n")
 
     def getfrenchtranslaton(self):
-        uClient = uReq(self.entofr_url)
-        page_html = uClient.read()
-        uClient.close()
-
-        page_soup = soup(page_html, "html.parser")
-
-        frenchword = page_soup.find("a", class_="dictLink featured")
+        frenchword = self.page_soup_entofr.find("a", class_="dictLink featured")
         frenchword = frenchword.text.strip().split(" ", 1)[0]
 
         print(f"{self.word} in French is: {frenchword}")
--- scripts/frenchword.py
+++ scripts/frenchword.py
@@ -1,25 +1,10 @@
 from urllib.request import urlopen as uReq
-from bs4 import BeautifulSoup as soup
+from bs4 import BeautifulSoup as BeautifulSoup
 import sys
-
 
 class FrenchWord:
 
     def __init__(self, word):
-        self.word = word
-        self.my_url = (f"https://www.linguee.com/english-french/search?source=auto&query={self.word}")
-
-        uClient = uReq(self.my_url)
-        page_html = uClient.read()
-        uClient.close()
-
-        self.page_soup = soup(page_html, "html.parser")
-    
-    def test(self):
-        pass
-
-    def getworddefinition(self):
-        worddefinition = self.page_soup.find("", class_="")
-
-        return worddefinition.text.strip()
-
+        self.word = word.replace(" ", "").lower()
+        self.frtoen_url = (f"https://www.linguee.com/french-english/search?source=auto&query={self.word}")
+        self.fr_url = (f"https://www.dictionary.com/browse/{self.word}?s=ts")
